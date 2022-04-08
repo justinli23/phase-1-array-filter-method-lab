c8dcba1c819007aa52ef45a7e39ba19e8bbbb9f0
Justin Li <justinlit23@gmail.com>
2022-04-07 17:28:52 -0700

    commit tree 5f515cacc88c68a83bd09250f7f3e3b5bd68a0b4

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+const findMatching = (arr, searchTerm) => {
+    const result = arr.filter(
+        name => name.toUpperCase() === searchTerm.toUpperCase()
+    )
+    return result
+}
+
+const arr = ['Alex', 'Bob', 'Carl'];
+
+const fuzzyMatch = (arr, searchTerm) => {
+    const result = arr.filter(
+          driverName => driverName.slice(0, searchTerm.length) === searchTerm
+    )
+    return result
+}
+
+const matchName = (drivers, string) => {
+    const result = drivers.filter(
+        object => object.name == string
+    )
+    return result;
+}
+
+
+const sampleDrivers = [
+    {'name': 'Jason', 'hometown': 'Wonderland'},
+    {'name': 'Jessica', 'hometown': 'Tennessee'}
+]
+
+//Each driver object has two properties: name and hometown.
+//The function should return each element whose name property
+//matches the provided string argument.
